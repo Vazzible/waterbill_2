8155e6290626d600bc2f348fe23f726c9722ab9b
Vazzible <166862249+Vazzible@users.noreply.github.com>
2025-10-01 14:15:07 -0600

    Test Change

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,8 @@ import java.io.InputStreamReader;
 public class Main{
     public static void main(String[] args){
         Customer customer1 = new Customer();
+        customer1.customerInput();
+        customer1.calculateBill();
         customer1.setGallonsUsed(-1000);
         customer1.calculateBill();
         customer1.setGallonsUsed(-10000);
